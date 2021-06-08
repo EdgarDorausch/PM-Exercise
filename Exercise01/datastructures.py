d6bd9fec8c4e3b780c7879ff61d38ca974b15ea7
EdgarDorausch <login@doraus.ch>
2021-06-08 13:34:41 +0200

    Restructureing

--- Exercise01/datastructures.py
+++ Exercise01/datastructures.py
@@ -5,7 +5,7 @@ if TYPE_CHECKING:
 
 import numpy as np
 from abc import ABC, abstractmethod
-import matplotlib.pyplot as plt
+
 
 class Particle(ABC):
     def __init__(self, position: np.ndarray, properties: np.ndarray):
@@ -117,65 +117,3 @@ class VerletList:
                 self.verlet_list[particle_index].extend([
                     q_idx for q_idx in neighbor_cell_list if self.cell_list.get_particle_distance(q_idx, particle_index) <= self.r_total
                     ])
-
-
-
-
-
-class MyParticle(Particle):
-    def __init__(self):
-        super().__init__(np.random.uniform(size=[2]), np.random.randint(0,2,size=[1]))
-    
-    def evolve(self):
-        pass
-
-    def interact(self, other: Particle):
-        pass
-
-if __name__ == "__main__":
-    np.random.seed(0)
-    pl = [MyParticle() for _ in range(100)]
-
-    vl = VerletList(
-        pl,
-        pos_min=np.array([0.,0.]),
-        pos_max=np.array([1.,1.]),
-        r_cutoff=0.1,
-        r_skin=0.02
-    )
-
-    cl = vl.cell_list
-
-    print(vl.verlet_list[2])
-
-    print(cl.get_adjacent_cells([6,9]))
-
-    get_len = np.vectorize(lambda x: len(x), otypes=[int])
-
-    print(get_len(cl.cell_list)[::-1,:])
-
-    cell = cl.cell_list[2,1]
-    verlet_neighbors = vl.verlet_list[2]
-
-    colors = ['r' if i in verlet_neighbors else 'b'  for i in range(100)]
-
-    colors[2] = 'g'
-
-    circle2 = plt.Circle(cl.position_list[::-1,2], vl.r_total, color='b', fill=False)
-
-    ax = plt.gca()
-    ax.cla() # clear things for fresh plot
-
-    ax.scatter(y=cl.position_list[0,:], x=cl.position_list[1,:], c=colors)
-    ax.set_xticks(np.arange(0, 1, step=0.1))
-    ax.set_yticks(np.arange(0, 1, step=0.1))
-    ax.grid(True)
-    ax.add_patch(circle2)
-    plt.show()
-
-
-
-
-
-
-        
